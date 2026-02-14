84038476c0fdb1248c1c0cf7b0e53382085d6ae3
Panuinc <panuwat.ja.forwork@gmail.com>
2026-02-14 18:24:18 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   /* config options here */
   reactCompiler: true,
   transpilePackages: ["@heroui/react"],
+  experimental: {
+    optimizePackageImports: ["@heroui/react"],
+  },
 };
 
 export default nextConfig;
